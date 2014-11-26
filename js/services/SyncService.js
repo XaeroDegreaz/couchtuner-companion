686440f220e994305ab7383e0e40ed21cd192cfb
Jerome Doby <xaerodegreaz@gmail.com>
2014-11-26 01:08:27 -0600

    #7 cleaning up the sync service objects's scope

--- js/services/SyncService.js
+++ js/services/SyncService.js
@@ -1,11 +1,34 @@
 (function () {
     app.service('SyncService', function () {
-        var data = {};
+        var data = {
+            bookmarks: [],
+            history: [],
+            settings: []
+        };
+
+        function sync(key, callback) {
+            var payload = {};
+            payload[key] = data[key];
+
+            if (!payload[key]) {
+                payload[key] = [];
+            }
+
+            chrome.storage.sync.set(payload, function () {
+                if (callback) {
+                    callback();
+                }
+            });
+        }
+
+        function clear() {
+            chrome.storage.sync.clear();
+        }
 
         return {
             initialize: function (callback) {
                 console.log("Initializing sync service...");
-                var sync = this.sync;
+                //# Retrieve all data at once.
                 chrome.storage.sync.get(null, function (storedData) {
                     data = storedData;
                     var query = Enumerable.from(data['bookmarks']);
@@ -22,6 +45,7 @@
                     }
                 });
             },
+
             removeBookmark: function (bookmarkIndex) {
                 //# We null here instead of splice so that bookmark buttons will not colide
                 //# due to the potential of assigning a duplicate bookmarkIndex on the click event.
@@ -29,14 +53,16 @@
                 //# is loaded on the next page load, we will cull the null keys at that time
                 //# since we don't need any multiple-page-load-persistent keys.
                 data['bookmarks'][bookmarkIndex] = null;
-                this.sync('bookmarks');
+                sync('bookmarks');
                 return data['bookmarks'];
             },
+
             addBookmark: function (bookmarkObject) {
                 data['bookmarks'].push(bookmarkObject);
-                this.sync('bookmarks');
+                sync('bookmarks');
                 return data['bookmarks'];
             },
+
             getSettings: function () {
                 if (!data["settings"]) {
                     return (data["settings"] = {
@@ -46,10 +72,7 @@
                 }
                 return data["settings"];
             },
-            setSetting: function (setting, value) {
-                data["settings"][setting] = value;
-                this.sync("settings");
-            },
+
             getHistory: function () {
                 if (!data["history"]) {
                     return (data["history"] = []);
@@ -59,46 +82,11 @@
                 return data["history"];
             },
 
-            setHistory: function (history) {
-                return data["history"] = history;
-            },
-
             getBookmarks: function () {
                 if (!data["bookmarks"]) {
                     return (data["bookmarks"] = []);
                 }
                 return data["bookmarks"];
-            },
-
-            setBookmarks: function (bookmarks) {
-                return data["bookmarks"] = bookmarks;
-            },
-
-            sync: function (key, callback) {
-                var payload = {};
-                payload[key] = data[key];
-
-                if (!payload[key]) {
-                    payload[key] = [];
-                }
-
-                chrome.storage.sync.set(payload, function () {
-                    if (callback) {
-                        callback();
-                    }
-                });
-            },
-
-            retrieve: function (key, callback) {
-                chrome.storage.sync.get(key, function (items) {
-                    //console.log("Retrieve: " + items);
-                    data[key] = items[key];
-                    callback(items);
-                });
-            },
-
-            clear: function () {
-                chrome.storage.sync.clear();
             }
         }
     });
