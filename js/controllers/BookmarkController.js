5c4166d8e1a86e1623a35080057097581ab182ee
Jerome Doby <xaerodegreaz@gmail.com>
2014-12-23 22:10:24 -0600

    Initial workings of the client side for the TVRage API.

--- js/controllers/BookmarkController.js
+++ js/controllers/BookmarkController.js
@@ -14,7 +14,33 @@
             function getSortedBookmarks() {
                 var query = Enumerable.from(SyncService.getBookmarks());
                 $scope.bookmarks = query.where("$ !== null").orderBy("$.name").toArray();
+
+               // $.each($scope.bookmarks, function (index, bookmark) {
+                    var bookmark = $scope.bookmarks[0];
+                    bookmark.nextUp = (bookmark.nextUp) ? bookmark.nextUp : "Loading...";
+                    //# TODO Add some sort of daily /weekly time limits for re-requesting data.
+                    $.get("http://localhost:8080/demo?showName=" + bookmark.name)
+                     .success(function (data) {
+                            $scope.$apply(function(){
+                                console.log(data);
+                                var date = data.childNodes[0].childNodes[5].innerHTML;
+                                var season = data.childNodes[0].childNodes[7].innerHTML;
+                                var episode = data.childNodes[0].childNodes[3].innerHTML;
+                                bookmark.nextUp = date;
+                                bookmark.bannerString = season+"x"+episode+" - "+date;
+                            });
+                     });
+                //});
             }
+
+            $scope.getLatestEpisode = function(bookmark) {
+                bookmark.nextUp = "No information available...";
+                $.get("http://localhost:8080/demo?showName=" + bookmark.name)
+                    .success(function (data) {
+                        console.log(data);
+                        bookmark.nextUp = data.childNodes[0].childNodes[5].innerHTML;
+                    });
+            };
 
             $scope.onBookmarkRemove = function ($event, bookmark) {
                 //# Need unsorted list for our indexes.
