49f0651441e7fd3a73225f467819b0823e451613
Jerome Doby <xaerodegreaz@gmail.com>
2015-05-01 22:45:54 -0500

    #13 - Rework of front end that should display next episode dates if available

--- js/controllers/BookmarkController.js
+++ js/controllers/BookmarkController.js
@@ -15,32 +15,45 @@
                 var query = Enumerable.from(SyncService.getBookmarks());
                 $scope.bookmarks = query.where("$ !== null").orderBy("$.name").toArray();
 
-               // $.each($scope.bookmarks, function (index, bookmark) {
-                    var bookmark = $scope.bookmarks[0];
+                $.each($scope.bookmarks, function (index, bookmark) {
+                    //var bookmark = $scope.bookmarks[0];
                     bookmark.nextUp = (bookmark.nextUp) ? bookmark.nextUp : "Loading...";
                     //# TODO Add some sort of daily /weekly time limits for re-requesting data.
-                    $.get("http://localhost:8080/demo?showName=" + bookmark.name)
-                     .success(function (data) {
-                            $scope.$apply(function(){
+                    $.get("http://localhost:8080/" + bookmark.name)
+                        .success(function (data) {
+                            $scope.$apply(function () {
                                 console.log(data);
-                                var date = data.childNodes[0].childNodes[5].innerHTML;
-                                var season = data.childNodes[0].childNodes[7].innerHTML;
-                                var episode = data.childNodes[0].childNodes[3].innerHTML;
-                                bookmark.nextUp = date;
-                                bookmark.bannerString = season+"x"+episode+" - "+date;
+                                bookmark.bannerString = data;
+                                bookmark.dateCompare = getDateCompareInt(data);
                             });
-                     });
-                //});
+                        });
+                });
             }
 
-            $scope.getLatestEpisode = function(bookmark) {
-                bookmark.nextUp = "No information available...";
-                $.get("http://localhost:8080/demo?showName=" + bookmark.name)
-                    .success(function (data) {
-                        console.log(data);
-                        bookmark.nextUp = data.childNodes[0].childNodes[5].innerHTML;
-                    });
-            };
+            function getDateCompareInt(date) {
+                var today = new Date();
+                var dd = today.getDate();
+                var mm = today.getMonth() + 1;
+                var yyyy = today.getFullYear();
+
+                if (dd < 10) {
+                    dd = '0' + dd
+                }
+
+                if (mm < 10) {
+                    mm = '0' + mm
+                }
+
+                today = yyyy + '' + mm + '' + dd;
+                date = date.replace(/-/g, '');
+                if (date < today) {
+                    return -1;
+                } else if (date == today) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            }
 
             $scope.onBookmarkRemove = function ($event, bookmark) {
                 //# Need unsorted list for our indexes.
